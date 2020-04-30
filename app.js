2f15a1c965524840424373be8cdba53048b27b86
LLath <rothpatrick12@yahoo.de>
2020-04-30 23:03:44 +0200

    fix: remove 3rd party package

--- app.js
+++ app.js
@@ -1,34 +1,26 @@
 const http = require("http");
-const createHandler = require("github-webhook-handler");
-const handler = createHandler({ path: "/webhook", secret: "" });
+const crypto = require("crypto");
+
+const SECRET = "";
 
 http
   .createServer((req, res) => {
-    handler(req, res, (err) => {
-      res.statusCode = 404;
-      res.sendDate("no such location");
+    req.on("data", (chunk) => {
+      const signature = `sha1=${crypto
+        .createHmac("sha1", SECRET)
+        .update(chunk)
+        .digest("hex")}`;
+
+      const isAllowed = req.headers["x-hub-signature"] === signature;
+
+      const body = JSON.parse(chunk);
+
+      const isMaster = body?.ref === "refs/heads/master";
+
+      if (isAllowed && isMaster) {
+        console.log("You pushed something important");
+      }
     });
+    res.end();
   })
-  .listen(7777);
-
-handler.on("error", (err) => {
-  console.error("Error", err.message);
-});
-
-handler.on("push", (event) => {
-  console.log(
-    "Received a push event for %s to %s",
-    event.payload.repository.name,
-    event.payload.ref
-  );
-});
-
-handler.on("issues", function (event) {
-  console.log(
-    "Received an issue event for %s action=%s: #%d %s",
-    event.payload.repository.name,
-    event.payload.action,
-    event.payload.issue.number,
-    event.payload.issue.title
-  );
-});
+  .listen(8080);
